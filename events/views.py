a85df5bb50b390c782d433f5a36264183aa00f44
olehmatskiv <85799629+olehmatskiv@users.noreply.github.com>
2022-06-04 23:20:54 +0300

    create blog.html and showes events on page

--- events/views.py
+++ events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+
+def home(request):
+    events = Event.objects
+    return render(request, 'events/index.html' , {'events' : events})
--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import events.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',events.views.home , name = 'home'),
+    path('posts/' , include('blog.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
